359fdec30fb8cbc1d046e86f12777df103b8f5fc
Vladimir Olennikov <ath31st@yandex.ru>
2023-11-29 17:24:01 +0300

    added javadoc for PromoCodeRepository

--- src/main/java/bot/farm/redeemer/repository/PromoCodeRepository.java
+++ src/main/java/bot/farm/redeemer/repository/PromoCodeRepository.java
@@ -5,8 +5,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Repository interface for interacting with PromoCode entities in the database.
+ */
 @Repository
 public interface PromoCodeRepository extends JpaRepository<PromoCode, Long> {
+
+  /**
+   * Checks if a promo code exists in a case-insensitive manner.
+   *
+   * @param code The promo code to check.
+   * @return {@code true} if the promo code exists, {@code false} otherwise.
+   */
   @Query("select (count(p) > 0) from PromoCode p where upper(p.code) = upper(?1)")
   boolean existsByCodeIgnoreCase(String code);
 }
