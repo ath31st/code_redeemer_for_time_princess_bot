ad98f2ea2d71c0fc176eb7b0b850e7491520cc6a
Vladimir Olennikov <ath31st@yandex.ru>
2023-11-30 16:21:59 +0300

    added javadoc for PromoCodeService and RegExpression

--- src/main/java/bot/farm/redeemer/service/PromoCodeService.java
+++ src/main/java/bot/farm/redeemer/service/PromoCodeService.java
@@ -7,17 +7,31 @@ import bot.farm.redeemer.util.RegExpression;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+/**
+ * Service class for handling operations related to promo codes.
+ */
 @Service
 @RequiredArgsConstructor
 public class PromoCodeService {
   private final PromoCodeRepository promoCodeRepository;
 
+  /**
+   * Saves a new promo code in the database.
+   *
+   * @param code The promo code to be saved.
+   */
   public void savePromoCode(String code) {
     PromoCode pc = new PromoCode();
     pc.setCode(code);
     promoCodeRepository.save(pc);
   }
 
+  /**
+   * Checks if a promo code already exists in a case-insensitive manner.
+   * Throws an exception if the promo code already exists.
+   *
+   * @param code The promo code to check.
+   */
   public void checkExistsPromoCode(String code) {
     if (promoCodeRepository.existsByCodeIgnoreCase(code)) {
       throw new PromoCodeException(
@@ -25,6 +39,12 @@ public class PromoCodeService {
     }
   }
 
+  /**
+   * Checks if a promo code has the correct format (only English letters and digits).
+   * Throws an exception if the format is incorrect.
+   *
+   * @param code The promo code to check.
+   */
   public void checkCorrectPromoCode(String code) {
     if (!code.matches(RegExpression.PROMO_CODE)) {
       throw new PromoCodeException(
--- src/main/java/bot/farm/redeemer/util/RegExpression.java
+++ src/main/java/bot/farm/redeemer/util/RegExpression.java
@@ -2,6 +2,9 @@ package bot.farm.redeemer.util;
 
 import lombok.experimental.UtilityClass;
 
+/**
+ * Utility class containing regular expressions for validation purposes.
+ */
 @UtilityClass
 public class RegExpression {
   public static final String PROMO_CODE = "[A-Za-z0-9]{2,50}";
